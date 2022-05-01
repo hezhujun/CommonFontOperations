e83667fafaa520b5e12fe815a171837e40b142be
hezhujun <1214507717@qq.com>
2022-05-01 15:58:16 +0800

    use cocoapods

--- CommonFontOperations/ViewController.swift
+++ CommonFontOperations/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let subView = UIView()
+        view.addSubview(subView)
+        subView.backgroundColor = .blue
+        
+        subView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 
 
